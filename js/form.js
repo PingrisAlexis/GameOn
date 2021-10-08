bc8b2a51a37b0264a74d778be90d008c977f4472
Alexis <pingrisalexis@gmail.com>
2021-10-08 09:55:37 +0200

    Refactoring valid/invalid form input

--- js/form.js
+++ js/form.js
@@ -18,83 +18,78 @@ const infoBirthdate = document.getElementById("info-birthdate");
 const infoNbrTournament = document.getElementById("info-nbr-tournament");
 const infoCitiesCheckboxes = document.getElementById("info-checkbox");
 
+const regexName = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
+
+//INVALID INPUT
+function invalidControl(selector, errorMessage) {
+	selector.textContent = errorMessage;
+	selector.style.color = "red";
+	selector.previousElementSibling.style.border = "solid red 0.19rem";
+
+	return false;
+}
+
+//VALID INPUT
+function validControl(selector, message) {
+	selector.textContent = message;
+	selector.style.color = "green";
+	selector.previousElementSibling.style.border = "solid green 0.19rem";
+
+	return selector.previousElementSibling.value;
+}
 //CONTROL FIRSTNAME
 function controlFirstname() {
 
 	const firstname = document.getElementById("user-firstname");
-	const regexName = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
 	const testFirstname = regexName.test(firstname.value);
-			
-	if (firstname.value === "") {
-			infoFirstname.textContent = "Prénom requis";
-			infoFirstname.style.color = "red";
-			infoFirstname.previousElementSibling.style.border = "solid red 0.19rem";
 
-		return false;
-			
+	if (firstname.value === "") {
+
+		return invalidControl(infoFirstname, "Prénom requis");
+
 	} 
 	else if (firstname.value.trim().length < 2) {
-			infoFirstname.textContent = "Le prénom doit avoir au moins deux caractéres";
-			infoFirstname.style.color = "red";
-			infoFirstname.previousElementSibling.style.border = "solid red 0.19rem";
 
-		return false;
-		
+		return invalidControl(infoFirstname, "Le prénom doit avoir au moins deux caractéres");
+
 	} 
 	else if (testFirstname === false){
-			infoFirstname.textContent = "Format incorrect";
-			infoFirstname.style.color = "red";
-			infoFirstname.previousElementSibling.style.border = "solid red 0.19rem";
 
-		return false ;
-		
+		return invalidControl(infoFirstname, "Format incorrect");
+
 	} 
 	else {
-			infoFirstname.textContent = "Prénom validé";
-			infoFirstname.style.color = "green";
-			infoFirstname.previousElementSibling.style.border = "solid green 0.19rem";
-		
-		return firstname.value;
+
+		return validControl(infoFirstname, "Prénom validé");
+
 	}
-};
+}
 
 //CONTROL LASTNAME
 function controlLastname() {
 
 	const lastname = document.getElementById("user-lastname");
-	const regexName = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
 	const testLastname = regexName.test(lastname.value);
 
 	if (lastname.value === "") {
-			infoLastname.textContent = "Nom requis";
-			infoLastname.style.color = "red";
-			infoLastname.previousElementSibling.style.border = "solid red 0.19rem";
-			
-		return false;
-	
+
+		return invalidControl(infoLastname, "Nom requis");
+
 	} 
 	else if (lastname.value.trim().length < 2) {
-			infoLastname.textContent = "Le nom doit avoir au moins deux caractéres";
-			infoLastname.style.color = "red";
-			infoLastname.previousElementSibling.style.border = "solid red 0.19rem";
-		
-		return false;
-		
+
+		return invalidControl(infoLastname, "Le nom doit avoir au moins deux caractéres");
+
 	} 
 	else if (testLastname === false){
-			infoLastname.textContent = "Format incorrect";
-			infoLastname.style.color = "red";
-			infoLastname.previousElementSibling.style.border = "solid red 0.19rem";
-			
-		return false;
-	
+
+		return invalidControl(infoLastname, "Format incorrect");
+
 	} 
 	else {
-			infoLastname.textContent = "Nom validé";
-			infoLastname.style.color = "green";
-			infoLastname.previousElementSibling.style.border = "solid green 0.19rem";
-			
-		return lastname.value;
+
+		return validControl(infoLastname, "Nom validé");
+
 	}
 }
 
@@ -106,27 +101,19 @@ function controlEmail () {
 	const testEmail = regexEmail.test(email.value);
 	
 	if (email.value === "") {
-			infoEmail.textContent = "E-mail requis";
-			infoEmail.style.color = "red";
-			infoEmail.previousElementSibling.style.border = "solid red 0.19rem";
-			
-		return false;
+
+		return invalidControl(infoEmail, "E-mail requis");
 
 	} 
 	else if (testEmail === false){
-			infoEmail.textContent = "Format incorrect";
-			infoEmail.style.color = "red";
-			infoEmail.previousElementSibling.style.border = "solid red 0.19rem";
-			
-		return false;
+
+		return invalidControl(infoEmail, "Format incorrect");
 
 	} 
 	else {
-			infoEmail.textContent = "E-mail validé";
-			infoEmail.style.color = "green";
-			infoEmail.previousElementSibling.style.border = 'solid green 0.19rem';
-			
-		return email.value;
+
+		return validControl(infoEmail, "E-mail validé");
+
 	}
 }
 
@@ -136,26 +123,19 @@ function controlBirthdate () {
 	const birthdate = document.querySelector("#user-birthdate");
 
 	if (birthdate.value === "") {
-			infoBirthdate.textContent = "Date de naissance requise";
-			infoBirthdate.style.color = "red";
-			infoBirthdate.previousElementSibling.style.border = "solid red 0.19rem";
-			
-		return false;
+
+		return invalidControl(infoBirthdate, "Date de naissance requise");
+
 	} 
 	else if (birthdate.value.trim().length <10){
-			infoBirthdate.textContent = "Format incorrect";
-			infoBirthdate.style.color = "red";
-			infoBirthdate.previousElementSibling.style.border = "solid red 0.19rem";
-			
-		return false;
+
+		return invalidControl(infoBirthdate, "Format incorrect");
+
 	}
-	
 	else {
-			infoBirthdate.textContent = "Date de naissance validé";
-			infoBirthdate.style.color = "green";
-			infoBirthdate.previousElementSibling.style.border = "solid green 0.19rem";
-			
-		return birthdate.value;
+
+		return validControl(infoBirthdate, "Date de naissance validé");
+
 	}
 }
 
@@ -163,11 +143,9 @@ function controlBirthdate () {
 function controlNbrTournament () {
 
 	if (nbrTournament.value === "") {
-			infoNbrTournament.textContent = "Nombre de participations requise";
-			infoNbrTournament.style.color = "red";
-			infoNbrTournament.previousElementSibling.style.border = "solid red 0.19rem";
-	
-		return false;
+
+		return invalidControl(infoNbrTournament, "Nombre de participations requise");
+
 	} 
 	else if (parseInt(nbrTournament.value) > 0 && arrayCitiesCheckbox.length <= 0){
 			infoCitiesCheckboxes.textContent = "Veuillez renseigner au moins une ville";
@@ -176,6 +154,7 @@ function controlNbrTournament () {
 		return false;
 	}
 	else {
+
 			infoNbrTournament.textContent = "Nombre de participations validé";
 			infoNbrTournament.style.color = "green";
 			infoNbrTournament.previousElementSibling.style.border = "solid green 0.19rem";
@@ -251,7 +230,7 @@ function validation(event){
 	event.preventDefault();
 	
 	let validFirstname = controlFirstname();
-	let validLastname = controlLastname();	
+	let validLastname = controlLastname();
 	let validEmail = controlEmail();
 	let validBirthdate = controlBirthdate();
 	let validNbrTournament = controlNbrTournament();
@@ -275,3 +254,4 @@ function validation(event){
 		displayConfirmationModal();
 	}
 }
+
